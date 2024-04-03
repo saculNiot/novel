3e8849ce9aac24c9ad0f17b5b35edd1cf0ebe633
TamingSari <gohsyang@gmail.com>
2024-04-04 01:55:43 +0800

    feat: Removed the markdown rule

--- packages/headless/src/extensions/index.ts
+++ packages/headless/src/extensions/index.ts
@@ -35,14 +35,7 @@ const simpleExtensions = [
     multicolor: true,
   }),
   Markdown.configure({
-    html: true, // Allow HTML input/output
-    tightLists: true, // No <p> inside <li> in markdown output
-    tightListClass: "tight", // Add class to <ul> allowing you to remove <p> margins when tight
-    bulletListMarker: "-", // <li> prefix in markdown output
-    linkify: true, // Create links from "https://..." text
-    breaks: true, // New lines (\n) in markdown input are converted to <br>
     transformPastedText: true, // Allow to paste markdown text in the editor
-    transformCopiedText: false // Copied text is transformed to markdown
   }),
   CustomKeymap,
   GlobalDragHandle.configure({
